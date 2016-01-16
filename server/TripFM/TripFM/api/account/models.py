7391e050cad707be912ca42286d58f32999333d4
luoyuxiang <luoyuxiang@wecash-mail.com.cn>
2016-01-16 19:24:32 +0800

    addToken and modify the AccountToken model

--- server/TripFM/TripFM/api/account/models.py
+++ server/TripFM/TripFM/api/account/models.py
@@ -5,5 +5,8 @@ from django.contrib.auth.models import User
 
 class AccountToken(models.Model):
 	user = models.ForeignKey(User)
-	access_token = models.CharField(max_length=50)
-	refresh_token = models.CharField(max_length=50)
+	access_token = models.CharField(max_length=500)
+	refresh_token = models.CharField(max_length=500)
+	token_type = models.CharField(max_length=500)
+	last_authenticated = models.CharField(max_length=500)
+	expires_in = models.CharField(max_length=500)
