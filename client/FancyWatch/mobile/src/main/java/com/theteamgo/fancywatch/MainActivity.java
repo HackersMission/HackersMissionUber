80ce9dbc880e75479e98cf269e62dba236a87277
Jesse <wangjiexi@pku.edu.cn>
2016-01-16 16:04:39 +0800

    implement message transformation interfaces

--- client/FancyWatch/mobile/src/main/java/com/theteamgo/fancywatch/MainActivity.java
+++ client/FancyWatch/mobile/src/main/java/com/theteamgo/fancywatch/MainActivity.java
@@ -5,11 +5,40 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.mobvoi.android.common.ConnectionResult;
+import com.mobvoi.android.common.api.MobvoiApiClient;
+//import com.mobvoi.android.common.api.MobvoiApiClient.ConnectionCallbacks;
+import com.mobvoi.android.common.api.MobvoiApiClient.OnConnectionFailedListener;
+//import com.mobvoi.android.common.api.ResultCallback;
+//import com.mobvoi.android.common.data.FreezableUtils;
+//import com.mobvoi.android.wearable.Asset;
+import com.mobvoi.android.wearable.DataApi;
+//import com.mobvoi.android.wearable.DataApi.DataItemResult;
+//import com.mobvoi.android.wearable.DataEvent;
+import com.mobvoi.android.wearable.DataEventBuffer;
+import com.mobvoi.android.wearable.MessageApi;
+//import com.mobvoi.android.wearable.MessageApi.SendMessageResult;
+import com.mobvoi.android.wearable.MessageEvent;
+import com.mobvoi.android.wearable.Node;
+import com.mobvoi.android.wearable.NodeApi;
+//import com.mobvoi.android.wearable.PutDataMapRequest;
+//import com.mobvoi.android.wearable.PutDataRequest;
+import com.mobvoi.android.wearable.Wearable;
+
+public class MainActivity extends AppCompatActivity implements DataApi.DataListener,
+        MessageApi.MessageListener, NodeApi.NodeListener, MobvoiApiClient.ConnectionCallbacks,
+        OnConnectionFailedListener{
+
+    private static final String TAG = "MainActivity";
+    private MobvoiApiClient mMobvoiApiClient;
+    private boolean mResolvingError = false;
+    private boolean mCameraSupported = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //LOGD(TAG, "created");
+        Log.d(TAG, "created");
+        mMobvoiApiClient = new MobvoiApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
     @Override
@@ -48,5 +84,83 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    //
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "started");
+        if (!mResolvingError) {
+            mMobvoiApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        if (!mResolvingError) {
+            Wearable.DataApi.removeListener(mMobvoiApiClient, this);
+            Wearable.MessageApi.removeListener(mMobvoiApiClient, this);
+            Wearable.NodeApi.removeListener(mMobvoiApiClient, this);
+            mMobvoiApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+
+    // Ticwatch API
+    @Override
+    public void onConnected(Bundle bundle) {
+        mResolvingError = false;
+        Log.d(TAG, "connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "connection failed");
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEventBuffer) {
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        Log.d(TAG, "onMessageReceived() A message from watch was received:" + messageEvent
+                .getRequestId() + " " + messageEvent.getPath());
+    }
+
+    @Override
+    public void onPeerConnected(Node node) {
+        Log.d(TAG, "onPeerConnected: " + node);
+    }
+
+    @Override
+    public void onPeerDisconnected(Node node) {
+        Log.d(TAG, "onPeerDisconnected: " + node);
+    }
+
+
+    private static void LOGD(final String tag, String message) {
+        if (Log.isLoggable(tag, Log.DEBUG)) {
+            Log.d(tag, message);
+        }
     }
 }
